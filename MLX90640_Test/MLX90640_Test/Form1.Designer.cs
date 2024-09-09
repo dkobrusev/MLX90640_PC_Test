63580cd89f4d110f457da5efc4f7a35e1a536b4e
dkobrusev <165299132+dkobrusev@users.noreply.github.com>
2024-09-09 20:47:11 +0300

    Add files via upload

---
+++ MLX90640_Test/MLX90640_Test/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace MLX90640_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comPortList = new System.Windows.Forms.ComboBox();
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.comPort = new System.IO.Ports.SerialPort(this.components);
+            this.updTimer = new System.Windows.Forms.Timer(this.components);
+            this.comPortBauds = new System.Windows.Forms.ComboBox();
+            this.checkTimer = new System.Windows.Forms.Timer(this.components);
+            this.labelSerialPort = new System.Windows.Forms.Label();
+            this.thermalPic = new System.Windows.Forms.PictureBox();
+            this.scalePic = new System.Windows.Forms.PictureBox();
+            this.lableTmax = new System.Windows.Forms.Label();
+            this.labelTmin = new System.Windows.Forms.Label();
+            this.thermalImageMinTemp = new System.Windows.Forms.Label();
+            this.thermalImageMaxTemp = new System.Windows.Forms.Label();
+            this.thermalImageAvgTemp = new System.Windows.Forms.Label();
+            this.thermalImageCenterTemp = new System.Windows.Forms.Label();
+            this.lableCentralTemp = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.thermalPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scalePic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comPortList
+            // 
+            this.comPortList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comPortList.FormattingEnabled = true;
+            this.comPortList.Location = new System.Drawing.Point(74, 12);
+            this.comPortList.Name = "comPortList";
+            this.comPortList.Size = new System.Drawing.Size(60, 21);
+            this.comPortList.TabIndex = 0;
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.Location = new System.Drawing.Point(250, 12);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(75, 21);
+            this.connectBtn.TabIndex = 1;
+            this.connectBtn.Text = "Connect";
+            this.connectBtn.UseVisualStyleBackColor = true;
+            this.connectBtn.Click += new System.EventHandler(this.connectBtn_Click);
+            // 
+            // comPort
+            // 
+            this.comPort.BaudRate = 115200;
+            this.comPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.comPort_DataReceived);
+            // 
+            // updTimer
+            // 
+            this.updTimer.Enabled = true;
+            this.updTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // comPortBauds
+            // 
+            this.comPortBauds.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comPortBauds.FormattingEnabled = true;
+            this.comPortBauds.Location = new System.Drawing.Point(142, 12);
+            this.comPortBauds.Name = "comPortBauds";
+            this.comPortBauds.Size = new System.Drawing.Size(100, 21);
+            this.comPortBauds.TabIndex = 2;
+            // 
+            // checkTimer
+            // 
+            this.checkTimer.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // labelSerialPort
+            // 
+            this.labelSerialPort.AutoSize = true;
+            this.labelSerialPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.labelSerialPort.Location = new System.Drawing.Point(11, 16);
+            this.labelSerialPort.Name = "labelSerialPort";
+            this.labelSerialPort.Size = new System.Drawing.Size(55, 13);
+            this.labelSerialPort.TabIndex = 3;
+            this.labelSerialPort.Text = "Serial Port";
+            // 
+            // thermalPic
+            // 
+            this.thermalPic.Location = new System.Drawing.Point(10, 40);
+            this.thermalPic.Name = "thermalPic";
+            this.thermalPic.Size = new System.Drawing.Size(640, 480);
+            this.thermalPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.thermalPic.TabIndex = 4;
+            this.thermalPic.TabStop = false;
+            // 
+            // scalePic
+            // 
+            this.scalePic.Location = new System.Drawing.Point(10, 550);
+            this.scalePic.Name = "scalePic";
+            this.scalePic.Size = new System.Drawing.Size(640, 20);
+            this.scalePic.TabIndex = 5;
+            this.scalePic.TabStop = false;
+            // 
+            // lableTmax
+            // 
+            this.lableTmax.AutoSize = true;
+            this.lableTmax.Location = new System.Drawing.Point(584, 531);
+            this.lableTmax.Name = "lableTmax";
+            this.lableTmax.Size = new System.Drawing.Size(33, 13);
+            this.lableTmax.TabIndex = 6;
+            this.lableTmax.Text = "Tmax";
+            // 
+            // labelTmin
+            // 
+            this.labelTmin.AutoSize = true;
+            this.labelTmin.Location = new System.Drawing.Point(7, 531);
+            this.labelTmin.Name = "labelTmin";
+            this.labelTmin.Size = new System.Drawing.Size(30, 13);
+            this.labelTmin.TabIndex = 7;
+            this.labelTmin.Text = "Tmin";
+            // 
+            // thermalImageMinTemp
+            // 
+            this.thermalImageMinTemp.AutoSize = true;
+            this.thermalImageMinTemp.Location = new System.Drawing.Point(36, 531);
+            this.thermalImageMinTemp.Name = "thermalImageMinTemp";
+            this.thermalImageMinTemp.Size = new System.Drawing.Size(34, 13);
+            this.thermalImageMinTemp.TabIndex = 8;
+            this.thermalImageMinTemp.Text = "12.34";
+            // 
+            // thermalImageMaxTemp
+            // 
+            this.thermalImageMaxTemp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.thermalImageMaxTemp.AutoSize = true;
+            this.thermalImageMaxTemp.Location = new System.Drawing.Point(616, 531);
+            this.thermalImageMaxTemp.Name = "thermalImageMaxTemp";
+            this.thermalImageMaxTemp.Size = new System.Drawing.Size(34, 13);
+            this.thermalImageMaxTemp.TabIndex = 9;
+            this.thermalImageMaxTemp.Text = "12,34";
+            // 
+            // thermalImageAvgTemp
+            // 
+            this.thermalImageAvgTemp.AutoSize = true;
+            this.thermalImageAvgTemp.Location = new System.Drawing.Point(313, 531);
+            this.thermalImageAvgTemp.Name = "thermalImageAvgTemp";
+            this.thermalImageAvgTemp.Size = new System.Drawing.Size(34, 13);
+            this.thermalImageAvgTemp.TabIndex = 10;
+            this.thermalImageAvgTemp.Text = "12.34";
+            // 
+            // thermalImageCenterTemp
+            // 
+            this.thermalImageCenterTemp.AutoSize = true;
+            this.thermalImageCenterTemp.BackColor = System.Drawing.Color.Transparent;
+            this.thermalImageCenterTemp.Location = new System.Drawing.Point(545, 16);
+            this.thermalImageCenterTemp.Name = "thermalImageCenterTemp";
+            this.thermalImageCenterTemp.Size = new System.Drawing.Size(34, 13);
+            this.thermalImageCenterTemp.TabIndex = 11;
+            this.thermalImageCenterTemp.Text = "36.66";
+            // 
+            // lableCentralTemp
+            // 
+            this.lableCentralTemp.AutoSize = true;
+            this.lableCentralTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.lableCentralTemp.Location = new System.Drawing.Point(465, 16);
+            this.lableCentralTemp.Name = "lableCentralTemp";
+            this.lableCentralTemp.Size = new System.Drawing.Size(76, 13);
+            this.lableCentralTemp.TabIndex = 12;
+            this.lableCentralTemp.Text = "Central Temp -";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 581);
+            this.Controls.Add(this.lableCentralTemp);
+            this.Controls.Add(this.thermalImageCenterTemp);
+            this.Controls.Add(this.thermalImageAvgTemp);
+            this.Controls.Add(this.thermalImageMaxTemp);
+            this.Controls.Add(this.thermalImageMinTemp);
+            this.Controls.Add(this.labelTmin);
+            this.Controls.Add(this.lableTmax);
+            this.Controls.Add(this.scalePic);
+            this.Controls.Add(this.thermalPic);
+            this.Controls.Add(this.labelSerialPort);
+            this.Controls.Add(this.comPortBauds);
+            this.Controls.Add(this.connectBtn);
+            this.Controls.Add(this.comPortList);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(675, 620);
+            this.MinimumSize = new System.Drawing.Size(675, 620);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MLX90640 Test";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.thermalPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scalePic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comPortList;
+        private System.Windows.Forms.Button connectBtn;
+        private System.IO.Ports.SerialPort comPort;
+        private System.Windows.Forms.Timer updTimer;
+        private System.Windows.Forms.ComboBox comPortBauds;
+        private System.Windows.Forms.Timer checkTimer;
+        private System.Windows.Forms.Label labelSerialPort;
+        private System.Windows.Forms.PictureBox thermalPic;
+        private System.Windows.Forms.PictureBox scalePic;
+        private System.Windows.Forms.Label lableTmax;
+        private System.Windows.Forms.Label labelTmin;
+        private System.Windows.Forms.Label lableCentralTemp;
+        public System.Windows.Forms.Label thermalImageMinTemp;
+        public System.Windows.Forms.Label thermalImageMaxTemp;
+        public System.Windows.Forms.Label thermalImageAvgTemp;
+        public System.Windows.Forms.Label thermalImageCenterTemp;
+    }
+}
+
---
+++ MLX90640_Test/MLX90640_Test/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Security.Cryptography.X509Certificates;
+using System.Reflection.Emit;
+
+
+namespace MLX90640_Test
+{
+    public partial class Form1 : Form
+    {
+        // Com port
+        int portsNumOld = 0;
+
+        // Buffer
+        const int bufferSize = 1542;
+
+        byte[] bufferTempData = new byte[bufferSize]; // Кадр + нач символ + номер кадра + Ta
+        int[] intTempData = new int[bufferSize / 2];
+        float[] floatTempData = new float[bufferSize / 2];
+
+        // Thermal image
+        const int W = 32;
+        const int H = 24;
+        const int pixScale = 20;
+
+        const int width = W * pixScale;
+        const int height = H * pixScale;
+        Bitmap bmpThermalImage = new Bitmap(width, height);
+
+        // Temperature scale
+        Bitmap bmpTempScale = new Bitmap(640, 20);
+        float minTemp = 1001.0f; // `C * 100
+        float maxTemp = 5002.0f;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------//
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            comPortList.Items.AddRange(SerialPort.GetPortNames());
+            comPortBauds.Items.AddRange(new[] { "9600", "19200", "38400", "57600", "115200" });
+            comPortBauds.SelectedIndex = comPortBauds.Items.Count - 1;
+
+            for (int i = 0; i < H; i++)
+            {
+                for (int j = 0; j < W; j++)
+                {
+                    for (int k = 0; k < pixScale; k++)
+                    {
+                        for (int l = 0; l < pixScale; l++)
+                        {
+                            bmpThermalImage.SetPixel(j * pixScale + k, i * pixScale + l, From1024ToRGB(Convert.ToInt32(1023.0f / 54.0f * (i + j))));
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < W * pixScale; j++)
+                {
+                    bmpTempScale.SetPixel(j, i, From1024ToRGB(Convert.ToInt32(1023.0f / (W * pixScale - 1) * j)));
+                }
+            }
+
+            thermalPic.Width = width;
+            thermalPic.Height = height;
+            thermalPic.Image = bmpThermalImage;
+            scalePic.Image = bmpTempScale;
+
+            thermalImageMaxTemp.Text = Convert.ToString(maxTemp / 100.0f);
+            thermalImageMinTemp.Text = Convert.ToString(minTemp / 100.0f);
+            thermalImageAvgTemp.Text = Convert.ToString((maxTemp + minTemp) / 200.0f);
+
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------//
+
+        private void connectBtn_Click(object sender, EventArgs e)
+        {
+            if (comPort.IsOpen) // Если ком порт открыт по нажатию кнопки
+            {
+                comPortDisconnect();        // Действия по закрытию порта
+            }
+            else                // Если ком порт закрыт по нажатию кнопки
+            {
+                comPortConnect();
+                if (comPort.BytesToRead > 0)
+                    comPort.ReadExisting();
+            }
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------//
+
+        private void timer1_Tick(object sender, EventArgs e)    // Таймер на 100 мл проверяет не появились ли новые компорты
+        {
+            string[] portNames = SerialPort.GetPortNames();
+
+            if (portNames.Length != portsNumOld)
+            {
+                comPortList.Items.Clear();
+                comPortList.Items.AddRange(portNames);
+                comPortList.SelectedIndex = portNames.Length - 1;
+            }
+            portsNumOld = portNames.Length;
+
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------//
+
+        private void timer1_Tick_1(object sender, EventArgs e)  // Таймер на 100 мл проверяет не отключились ли устройства
+        {
+            if (!comPort.IsOpen)
+            {
+                comPortDisconnect();    // Действия по закрытию порта
+            }
+
+
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------//
+
+        private void comPortDisconnect()    // Действия по закрытию порта
+        {
+            comPort.Close();                                    // Закрыть порт
+            connectBtn.Text = "Connect";                        // Изменить текст кнопки
+            comPortList.Enabled = comPortBauds.Enabled = true;  // Заблокировать изменения порта и скорости
+            updTimer.Enabled = true;                            // Вкл таймер отслеживания изменения кол-ва портов
+            checkTimer.Enabled = false;                         // Выкл таймер отслеживания отключения устройства
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------//
+
+        private void comPortConnect()    // Действия по закрытию порта
+        {
+            comPort.PortName = comPortList.SelectedItem.ToString();         // Задать выбранный ком порт
+            comPort.BaudRate = Convert.ToInt32(comPortBauds.SelectedItem);  // Установить порту заданную скорость
+            comPort.Open();                                                 // Открыть порт
+            connectBtn.Text = "Disconnect";                                 // Изменить текст кнопки
+            comPortList.Enabled = comPortBauds.Enabled = false;             // Запретить изменения порта и скорости
+            updTimer.Enabled = false;                                       // Выкл таймер отслеживания изменения кол-ва портов
+            checkTimer.Enabled = true;                                      // Вкл таймер отслеживания отключения устройства
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------//
+
+        private async void comPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (comPort.BytesToRead >= bufferSize)
+            {
+                Console.WriteLine(comPort.BytesToRead);
+                comPort.Read(bufferTempData, 0, bufferSize);
+
+                for (int i = 0; i < bufferSize / 2; i++)
+                {
+                    intTempData[i] = (Convert.ToInt16(bufferTempData[2 * i]) << 8) + (Convert.ToInt16(bufferTempData[2 * i + 1]));
+                    //floatTempData[i] = intTempData[i] / 100.0f;
+                    if (i > 1 && i < 770)
+                    {
+                        if (i == 2)
+                        {
+                            maxTemp = minTemp = intTempData[i];
+                        }
+                        else
+                        {
+                            if (intTempData[i] < minTemp)
+                            {
+                                minTemp = intTempData[i];
+                            }
+                            if (intTempData[i] > maxTemp)
+                            {
+                                maxTemp = intTempData[i];
+                            }
+                        }
+                    }
+                }
+
+                // Построение распределения в консоли
+                for (int i = 0; i < 24; i++)
+                {
+                    for (int j = 0; j < 32; j++)
+                    {
+                        Console.Write(intTempData[(i * 32 + j + 2)]);
+                        Console.Write('\t');
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+                Console.WriteLine("/-----------------------------------/");
+
+                // Обновление изображения на экране
+                updatePic(intTempData);
+            }
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------//
+        
+        // Преобразует значения от 0 до 1023 в 256 цветов от синего до красного
+        public Color From1024ToRGB(int hue)
+        {
+            int red = 0, green = 0, blue = 0;
+            if (hue < 0 || hue > 1023) // Защита от ошибок
+                return Color.Black;
+            else
+            {
+                switch (hue / 256)
+                {
+                    case 0:
+                        red = 0;
+                        green = hue;
+                        blue = 255;
+                        break;
+                    case 1:
+                        red = 0;
+                        green = 255;
+                        blue = 511 - hue; // 255 - (hue - 256)
+                        break;
+                    case 2:
+                        red = hue - 512;
+                        green = 255;
+                        blue = 0;
+                        break;
+                    case 3:
+                        red = 255;
+                        green = 1023 - hue; // 255 - (hue - 768)
+                        blue = 0;
+                        break;
+                }
+
+                return Color.FromArgb(255, red, green, blue);
+            }
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------//
+
+        // Обновление изображения на экране
+        public void updatePic(int[] tempData)
+        {
+            for (int i = 0; i < H; i++)
+            {
+                for (int j = 0; j < W; j++)
+                {
+                    for (int k = 0; k < pixScale; k++)
+                    {
+                        for (int l = 0; l < pixScale; l++)
+                        {
+                            // Закрашивает каждый пиксель определенным цветом (зависит от температуры)
+                            // Функция принимает массив температур (значение температуры умноженое на 100), преобразует в диапазон от min до max, где 0 - min, 1023 - max
+                            bmpThermalImage.SetPixel(j * pixScale + k, i * pixScale + l, From1024ToRGB(Convert.ToInt32(Convert.ToDouble(tempData[32 * i + j + 2] - minTemp) / (maxTemp - minTemp) * 1023.0f)));
+                        }
+                    }
+                }
+            }
+
+            // Обновление картинки
+            thermalPic.Image = bmpThermalImage;
+
+            // Махинации с обновлением значений температур (min, max, (min+max/2) and center)
+            thermalImageMinTemp.Invoke((MethodInvoker)(() => thermalImageMinTemp.Text = Convert.ToString(minTemp / 100.0f)));
+            thermalImageMaxTemp.Invoke((MethodInvoker)(() => thermalImageMaxTemp.Text = Convert.ToString(maxTemp / 100.0f)));
+            thermalImageAvgTemp.Invoke((MethodInvoker)(() => thermalImageAvgTemp.Text = Convert.ToString((maxTemp + minTemp) / 200.0f)));
+            thermalImageCenterTemp.Invoke((MethodInvoker)(() => thermalImageCenterTemp.Text = Convert.ToString(intTempData[367] / 100.0f)));
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------//
+    }
+}
